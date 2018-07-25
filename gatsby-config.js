f473f0ed04a8550c663fbeb5b893cd4365ba5e08
Tim Gorer <tgorer@fanatics.com>
2018-07-25 01:46:51 -0700

    implement States and Events

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Timothy Gorer Portfolio',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Card from '../components/Card';
+
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>My name is< br/>Timothy Gorer</h1>
+        <p>I am an iOS development intern at Fanatics. Today I will talk about what I did this summer.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+        <div className="Logos">
+          <img src={require('../images/logo-sketch.png')}
+          width="50" />
+          <img src={require('../images/logo-figma.png')}
+          width="50" />
+          <img src={require('../images/logo-studio.png')}
+          width="50" />
+          <img src={require('../images/logo-framer.png')}
+          width="50" />
+          <img src={require('../images/logo-react.png')}
+          width="50" />
+          <img src={require('../images/logo-swift.png')}
+          width="50" />
+        </div>
+        <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill="white">
+            <animate repeatCount="indefinite"
+            fill="freeze" attributeName="d" dur="10s"
+            values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
+            
+            M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
+
+            M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
+            
+            M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;"
+            />
+          </path>
+        </svg>
+      </div>
+    </div>
+    <div className="Cards">
+      <h2>11 courses, more coming</h2>
+      <div className="CardGroup">
+        <Card 
+        title="Design System"
+        text="10 sections"
+        image={require('../images/wallpaper.jpg')} />
+        <Card 
+        title="React for Designers"
+        text="12 sections"
+        image={require('../images/wallpaper2.jpg')} />
+        <Card 
+        title="Sound Design"
+        text="5 sections"
+        image={require('../images/wallpaper3.jpg')} />
+        <Card 
+        title="ARKit 2"
+        text="10 sections"
+        image={require('../images/wallpaper4.jpg')} />
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
+
