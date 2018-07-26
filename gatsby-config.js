e02fd7542352feaa4a0071d2e937b24e118b2de3
Tim Gorer <tgorer@fanatics.com>
2018-07-26 16:21:43 -0700

    implement GraphQL and Contenful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+
 module.exports = {
   siteMetadata: {
-    title: 'Timothy Gorer Portfolio',
+	title: 'Timothy Gorer Portfolio',
+	description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+	keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'ggb5eziaudfi',
+        accessToken: '6945f3b2401d2af7d783ef4823098db0fd816f94b375f95497436ceb8cb9388a'
+      }
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Card from '../components/Card';
+import Card from '../components/Card'
+import Section from '../components/Section'
+import Wave from '../components/Wave'
+import staticdata from '../../staticdata.json'
+import Cell from '../components/Cell';
+import styled from 'styled-components'
+
+const SectionCaption = styled.p`
+  font-weight: 600;
+  font-size: 10px;
+  text-transform: uppercase;
+  color: #94A4BA;
+  text-align: center;
+`
+
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
+
 
 const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
         <h1>My name is< br/>Timothy Gorer</h1>
-        <p>I am an iOS development intern at Fanatics. Today I will talk about what I did this summer.</p>
+        <p>I am an iOS development intern at Fanatics. Today I will talk about my summer accomplishments.</p>
         <Link to="/page-2/">Go to page 2</Link>
         <div className="Logos">
           <img src={require('../images/logo-sketch.png')}
@@ -23,20 +50,7 @@ const IndexPage = () => (
           <img src={require('../images/logo-swift.png')}
           width="50" />
         </div>
-        <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill="white">
-            <animate repeatCount="indefinite"
-            fill="freeze" attributeName="d" dur="10s"
-            values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-            
-            M0 86.3149C316 86.315 444 159.155 884 51.1554C1324 -56.8446 1320.29 34.1214 1538 70.4063C1814 116.407 2156 188.408 2560 86.315V232.317L0 232.316V86.3149Z;
-
-            M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-            
-            M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;"
-            />
-          </path>
-        </svg>
+        <Wave />
       </div>
     </div>
     <div className="Cards">
@@ -60,6 +74,21 @@ const IndexPage = () => (
         image={require('../images/wallpaper4.jpg')} />
       </div>
     </div>
+    <Section
+      image={require('../images/wallpaper2.jpg')}
+      logo={require('../images/logo-react.png')}
+      title="React for Designers"
+      text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+    />
+    <SectionCaption>12 sections - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {staticdata.cells.map(cell => (
+        <Cell 
+        title={cell.title}
+        image={cell.image} />
+      ))}
+    </SectionCellGroup>
+    
   </div>
 )
 
